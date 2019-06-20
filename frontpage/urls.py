7ffd4df0c4e626581fee3389e4585d9c488f5065
SkyVault <dustinneumann42@gmail.com>
2019-06-19 20:36:10 -0700

    Now have a basic project page route

--- frontpage/urls.py
+++ frontpage/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.FrontPage, name='frontpage'),
+    path('projects/<int:project_id>', views.ProjectView),
     path('', views.FrontPage, name='home'),
 ]
--- frontpage/views.py
+++ frontpage/views.py
@@ -4,6 +4,16 @@ from .models import Project
 
 def FrontPage(request):
     context = {
+        'APPNAME': "WorkingOn",
         'projects': Project.objects.all()
     }
     return render(request, 'frontpage.html', context)
+
+
+# TODO(Dustin): Probably need to move this, not sure where...
+def ProjectView(request, project_id):
+    context = {
+        'APPNAME': "WorkingOn",
+        'project': Project.objects.get(id=project_id)
+    }
+    return render(request, 'project.html', context)
