35bd240b348a66a2fc191d233587fea5dca89518
SkyVault <dustinneumann42@gmail.com>
2019-06-20 11:07:58 -0700

    Now list the users projects in the dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def UserDashboard(request):
     context = {
-
+        'current_user': request.user,
+        'projects': request.user.project_set.all()
     }
     return render(request, 'dashboard.html', context)
